9b64667afcd819fb731785fb54ae7f0bb3d92026
volkerschulz <github@volkerschulz.de>
2023-11-04 08:09:54 +0100

    Load js and css in correct order (common first)

--- templates/composer/v1/src/PluginName/Loader.php
+++ templates/composer/v1/src/PluginName/Loader.php
@@ -13,10 +13,10 @@ class Loader {
          * Enqueue admin scripts and styles
          */
         add_action( 'admin_enqueue_scripts', function() {
-            wp_enqueue_style( Config::PLUGIN_SLUG . '-admin', Config::get( 'plugin_dir_url' ) . 'css/admin.css', [], Config::get( 'version' ), 'all' );
-            wp_enqueue_script( Config::PLUGIN_SLUG . '-admin', Config::get( 'plugin_dir_url' ) . 'js/admin.js', [ 'jquery' ], Config::get( 'version' ), [ 'in_footer' => false ] );
             wp_enqueue_style( Config::PLUGIN_SLUG . '-common', Config::get( 'plugin_dir_url' ) . 'css/common.css', [], Config::get( 'version' ), 'all' );
             wp_enqueue_script( Config::PLUGIN_SLUG . '-common', Config::get( 'plugin_dir_url' ) . 'js/common.js', [ 'jquery' ], Config::get( 'version' ), [ 'in_footer' => false ] );
+            wp_enqueue_style( Config::PLUGIN_SLUG . '-admin', Config::get( 'plugin_dir_url' ) . 'css/admin.css', [ Config::PLUGIN_SLUG . '-common' ], Config::get( 'version' ), 'all' );
+            wp_enqueue_script( Config::PLUGIN_SLUG . '-admin', Config::get( 'plugin_dir_url' ) . 'js/admin.js', [ 'jquery', Config::PLUGIN_SLUG . '-common' ], Config::get( 'version' ), [ 'in_footer' => false ] );
         } );
         
 
@@ -24,10 +24,10 @@ class Loader {
          * Enqueue public scripts and styles
          */
         add_action( 'wp_enqueue_scripts', function() {
-            wp_enqueue_style( Config::PLUGIN_SLUG . '-public', Config::get( 'plugin_dir_url' ) . 'css/public.css', [], Config::get( 'version' ), 'all' );
-            wp_enqueue_script( Config::PLUGIN_SLUG . '-public', Config::get( 'plugin_dir_url' ) . 'js/public.js', [ 'jquery' ], Config::get( 'version' ), [ 'in_footer' => false ] );
             wp_enqueue_style( Config::PLUGIN_SLUG . '-common', Config::get( 'plugin_dir_url' ) . 'css/common.css', [], Config::get( 'version' ), 'all' );
             wp_enqueue_script( Config::PLUGIN_SLUG . '-common', Config::get( 'plugin_dir_url' ) . 'js/common.js', [ 'jquery' ], Config::get( 'version' ), [ 'in_footer' => false ] );
+            wp_enqueue_style( Config::PLUGIN_SLUG . '-public', Config::get( 'plugin_dir_url' ) . 'css/public.css', [ Config::PLUGIN_SLUG . '-common' ], Config::get( 'version' ), 'all' );
+            wp_enqueue_script( Config::PLUGIN_SLUG . '-public', Config::get( 'plugin_dir_url' ) . 'js/public.js', [ 'jquery', Config::PLUGIN_SLUG . '-common' ], Config::get( 'version' ), [ 'in_footer' => false ] );
         } );
 
         
